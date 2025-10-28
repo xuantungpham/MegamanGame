7f5e04163394ae5798e18fddaa1eeb94d0848eb5
xuantungpham <phamxuantung1422000@gmail.com>
2025-10-29 03:50:14 +0700

    Xong project

---
+++ MegamanGame/src/com/gamestudio/userinterface/InputManager.java
@@ -0,0 +1,28 @@
+
+package com.gamestudio.userinterface;
+
+import com.gamestudio.state.GameWorldState;
+import com.gamestudio.state.State;
+import java.awt.event.KeyEvent;
+
+public class InputManager {
+    
+    private State gameState;
+    
+    public InputManager(State state){
+        this.gameState = state;
+    }
+    
+    public void setState(State state) {
+        gameState = state;
+    }
+    
+    public void setPressedButton(int code){
+        gameState.setPressedButton(code);
+    }
+    
+    public void setReleasedButton(int code){
+        gameState.setReleasedButton(code);
+    }
+    
+}
